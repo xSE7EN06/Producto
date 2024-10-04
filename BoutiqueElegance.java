d88457b9b76fd83cbb390b4baa91ced5a42d867d
xSE7EN06 <agao6105@gmail.com>
2024-10-03 23:16:54 -0600

    feat(add-prenda): Agregar funcion para registrar prendas

--- BoutiqueElegance.java
+++ BoutiqueElegance.java
@@ -4,19 +4,21 @@ import java.util.Scanner;
 
 public class BoutiqueElegance {
     private static Scanner teclado;
+    private static int contador = 10;
     public static void main(String[] args) {
         teclado = new Scanner(System.in);
-        Prenda[] inventario = new Prenda[10];
-        inventario[0] = new Prenda("Vestido de Noche", "Diseñador A", 2022, 150.0);
-        inventario[1] = new Prenda("Camisa Casual", "Diseñador B", 2021, 50.0);
-        inventario[2] = new Prenda("Pantalón Deportivo", "Diseñador C", 2023, 70.0);
-        inventario[3] = new Prenda("Chaqueta de Cuero", "Diseñador D", 2020, 200.0);
-        inventario[4] = new Prenda("Falda Elegante", "Diseñador A", 2022, 80.0);
-        inventario[5] = new Prenda("Blusa de Seda", "Diseñador B", 2021, 60.0);
-        inventario[6] = new Prenda("Abrigo de Invierno", "Diseñador E", 2023, 300.0);
-        inventario[7] = new Prenda("Traje de Baño", "Diseñador F", 2023, 40.0);
-        inventario[8] = new Prenda("Sandalias de Verano", "Diseñador A", 2022, 35.0);
-        inventario[9] = new Prenda("Zapatos de Tacón", "Diseñador B", 2021, 120.0);
+        Prenda[] inventario = {
+            new Prenda("Vestido de Noche", "Diseñador A", 2022, 150.0),
+            new Prenda("Camisa Casual", "Diseñador B", 2021, 50.0),
+            new Prenda("Pantalón Deportivo", "Diseñador C", 2023, 70.0),
+            new Prenda("Chaqueta de Cuero", "Diseñador D", 2020, 200.0),
+            new Prenda("Falda Elegante", "Diseñador A", 2022, 80.0),
+            new Prenda("Blusa de Seda", "Diseñador B", 2021, 60.0),
+            new Prenda("Abrigo de Invierno", "Diseñador E", 2023, 300.0),
+            new Prenda("Traje de Baño", "Diseñador F", 2023, 40.0),
+            new Prenda("Sandalias de Verano", "Diseñador A", 2022, 35.0),
+            new Prenda("Zapatos de Tacón", "Diseñador B", 2021, 120.0)
+        };
 
         int opcion;
         boolean salir = false;
@@ -26,7 +28,7 @@ public class BoutiqueElegance {
             opcion = teclado.nextInt(); 
 
             switch (opcion) {
-                case 1:
+            case 1:
                 do{
                 System.out.println("Ingresa el nombre del diseñador");
                 String nombre = teclado.nextLine();
@@ -46,14 +48,35 @@ public class BoutiqueElegance {
                }while(!salir);
                 salir = false;
                 break;
-                case 2:
-
+            case 2:
+                double total = calcularTotal(inventario);
+                System.out.println("El precio total de todos los articulos es de: "+total);
                 break;
-                case 3:
+            case 3:
+            if(contador < inventario.length){
+                System.out.println("Ingresa el nombre de la prenda");
+                String nombrePrenda = teclado.nextLine();
 
+                System.out.println("Ingresa el nombre del disenador");
+                String nombreDisenador = teclado.nextLine();
+
+                System.out.println("Ingresa el año de coleccion");
+                int anio = teclado.nextInt();
+
+                System.out.println("Ingresa el precio");
+                double precio = teclado.nextDouble();
+                teclado.nextLine();
+
+                inventario[contador] = new Prenda(nombrePrenda, nombreDisenador, anio, precio);
+                contador++; // Incrementar el contador
+                System.out.println("Prenda agregada exitosamente.");
+
+            }else {
+                System.out.println("El inventario está lleno. No se pueden agregar más prendas.");
+            }
                 break;
-                case 4:
-
+            case 4:
+            salir = true;
                 break;
                 default:
                 System.out.println("No existe el valor favor de intentar de nuevo.");
